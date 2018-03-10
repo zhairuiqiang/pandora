1e3ee5b9ef224477ffa2a844960ac9b85fbc5801
zhairichard <zhairichard@DESKTOP-P3N9JK5>
2018-03-10 11:24:57 +0800

    test-step1

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@RequestMapping("/sts")
+	public String hello()
+	{
+		return "nihao";
+	}
+}
